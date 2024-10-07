1cb8623f7a7f92c60d921dedb2d503f1c7a4b52f
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-08 00:30:09 +0700

    Menchecek Beberapa Variabel Sekaligus

---
+++ 29 Menchecek Beberapa Variabel Sekaligus.php
@@ -0,0 +1,10 @@
+<?php
+$var1 = "PHP";
+$var2 = "isset";
+
+if (isset($var1, $var2)) {
+    echo "Kedua variabel telah di-set.";
+} else {
+    echo "Salah satu atau kedua variabel belum di-set.";
+}
+?>
