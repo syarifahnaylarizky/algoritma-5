a01b2631f676b09505ca2344b379a1483aef63c5
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-08 00:37:12 +0700

    kombinasi dgn fungsi lain

---
+++ 32 kombinasi dengan fungsi lain.php
@@ -0,0 +1,9 @@
+<?php
+$var4 = "";
+
+if (isset($var4) && !empty($var4)) {
+    echo "Variabel 'var4' di-set dan tidak kosong.";
+} else {
+    echo "Variabel 'var4' kosong atau belum di-set.";
+}
+?>
