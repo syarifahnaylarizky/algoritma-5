e0336c7794c4377d48bad4a61a683b7270c69af9
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 23:50:24 +0700

    jumlah argumen dinamis

---
+++ 17 Jumlah Argumen Dinamis.php
@@ -0,0 +1,10 @@
+<?php
+function namaFungsi($parameter = 'nilaiDefault') {
+    // kode fungsi di sini
+}
+function sapa($nama = 'pengunjung') {
+    echo "Halo, $nama!";
+}	
+
+sapa(); // Output: Halo, pengunjung!
+sapa('Dewi'); // Output: Halo, Dewi!
