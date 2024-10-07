7f82cc4c77d57bd17f4f8d838751fb5e4904382d
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 22:42:26 +0700

    default argumen

---
+++ 03 default argumen.php
@@ -0,0 +1,7 @@
+<?php
+function nama_bulan($bulan, $tahun = 2016) {
+	echo $bulan . ' ' . $tahun;
+}
+nama_bulan('Januari'); // Hasil Januari 2016
+echo "<hr>" ;
+nama_bulan('Nayla'); // Hasil Januari 2016
