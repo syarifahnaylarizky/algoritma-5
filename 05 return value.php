22a1a29cd7c21ae33820bbed27db880f560f2c8d
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 22:55:01 +0700

    return value

---
+++ 05 return value.php
@@ -0,0 +1,9 @@
+<?php
+// membuat fungsi
+function hitungUmur($thn_lahir, $thn_sekarang){
+  $umur = $thn_sekarang - $thn_lahir;
+  return $umur;
+}	
+
+echo "Umur kita adalah ". hitungUmur(1986, 2023) ." tahun";
+?>
