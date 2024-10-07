8a5deb7f7fb49d3b184dcff9ea7804feefa4d511
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 22:23:43 +0700

    first commit

--- 01 fungsi pada php.php
+++ 01 fungsi pada php.php
@@ -2,7 +2,7 @@
 // mmbuat fungsi
 function perkenalan(){
   echo "Assalamulaikum, ";
-  echo "Perkenalkan, nama saya Nayla<br/>";
+  echo "Perkenalkan, nama saya Naylarizky<br/>";
   echo "Senang berkenalan dengan anda<br/>";
 }
 // memanggil fungsi yang sudah dibuat
