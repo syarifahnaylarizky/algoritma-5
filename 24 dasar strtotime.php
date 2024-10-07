8a9b5709a744d2ab62b94276598597ffe4e739e7
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-08 00:18:06 +0700

    dasar strtotime

---
+++ 24 dasar strtotime.php
@@ -0,0 +1,7 @@
+<?php
+$timestamp = strtotime("now");
+echo $timestamp; // Menampilkan timestamp untuk waktu saat ini
+
+$timestamp = strtotime("10 September 2000");
+echo $timestamp; // Menampilkan timestamp untuk tanggal 10 September 2000
+?>
