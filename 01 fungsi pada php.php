bfcbbfddc79a15089ab32b274f3a3e07f0843255
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-07 22:16:00 +0700

    fungsi perkenalan

---
+++ 01 fungsi pada php.php
@@ -0,0 +1,14 @@
+<?php
+// mmbuat fungsi
+function perkenalan(){
+  echo "Assalamulaikum, ";
+  echo "Perkenalkan, nama saya Nayla<br/>";
+  echo "Senang berkenalan dengan anda<br/>";
+}
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+
+echo "<hr>";
+// memanggilnya lagi
+perkenalan();
+?>
