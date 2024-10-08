3ee28141c1a88b32f8e4a38c2adc19488e6eef13
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-08 07:47:09 +0700

    program hitung

--- 36 program hitung.php
+++ 36 program hitung.php
@@ -6,7 +6,7 @@
 <body>
 
 <form action="proses.php" method="get">
-    Nama: <input type="text" name="nama"><br>
+    Namaa: <input type="text" name="nama"><br>
     Email: <input type="text" name="email"><br>
 <input type="submit" value="Kirim">
 </form>
