4f42bff3dd0a83631e34b93fa585612f833e3db3
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-08 07:45:13 +0700

    program hitung

---
+++ 36 program hitung.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contoh Formulir GET di PHP</title>
+</head>
+<body>
+
+<form action="proses.php" method="get">
+    Nama: <input type="text" name="nama"><br>
+    Email: <input type="text" name="email"><br>
+<input type="submit" value="Kirim">
+</form>
+
+</body>
+</html>
