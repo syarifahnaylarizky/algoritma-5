a350f82a014f571ad14e6acf799fee9b1aaebbda
syarifahnaylarizky <raditsaid471@gmail.com>
2024-10-08 08:57:03 +0700

    parameter default

--- 07 parameter default.php
+++ 07 parameter default.php
@@ -2,17 +2,17 @@
 // mmbuat fungsi
 function perkenalan($nama, $salam="Assalamualaikum"){
   echo $salam.", ";
-  echo "Perkenalkan, nama kita ".$Nayla."<br/>";
+  echo "Perkenalkan, nama kita ".$nama."<br/>";
   echo "Senang berkenalan dengan anda<br/>";
-}
+}	
 
 // memanggil fungsi yang sudah dibuat
-perkenalan("Nayla", "Hi");
+perkenalan("pika", "Hi");
 
 echo "<hr>";
 
-$kita = "Nayla";
+$kita = "nayla";
 $ucapanSalam = "Selamat pagi";
 // memanggilnya lagi tanpa mengisi parameter salam
-perkenalan($saya);
+perkenalan("nayla");
 ?>
